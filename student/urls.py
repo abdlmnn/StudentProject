51ac7b6a7bc86c03d2aa15a0bf2c960d1175d14a
Mohammad Abdulmanan <mohammadabdulmanan24@gmail.com>
2025-03-30 20:42:56 +0800

    1st backend

--- student/urls.py
+++ student/urls.py
@@ -1,6 +1,17 @@
 from django.urls import path
-from .views import *
+from . import views
 
 urlpatterns = [
-    path('',index,name='index'),
+    path('',views.index,name='index'),
+    path('student/',views.student,name='student'),
+    path('course/',views.course,name='course'),
+    path('subject/',views.subject,name='subject'),
+    path('yearlevel/',views.yearlevel,name='yearlevel'),
+    path('studentyearlevel/',views.studentyearlevel,name='studentyearlevel'),
+    path('teacher/',views.teacher,name='teacher'),
+    path('attendance/',views.attendance,name='attendance'),
+    path('schedule/',views.schedule,name='schedule'),
+
+
+    path('student/add',views.add_student,name='add_student')
 ]
--- student/views.py
+++ student/views.py
@@ -1,9 +1,54 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import *
 # Create your views here.
 
 def main(request):
     return render(request,'base.html')
 
 def index(request):
-    return render(request,'student/index.html',{'title':'Home'})
+    return render(request,'student/index.html',{'title':'Dashboard'})
+
+def student(request):
+    return render(request, 'student/student.html',{'title':'Students','student_data': Student.objects.all(), 'course_data':Course.objects.all()})
+
+def course(request):
+    return render(request, 'student/course.html',{'title':'Course'})
+
+def subject(request):
+    return render(request, 'student/subject.html',{'title':'Subject'})
+
+def yearlevel(request):
+    return render(request, 'student/yearlevel.html',{'title':'Year Level'})
+
+def studentyearlevel(request):
+    return render(request, 'student/studentyearlevel.html',{'title':'Student Year Level'})
+
+def teacher(request):
+    return render(request, 'student/teacher.html',{'title':'Teacher'})
+
+def attendance(request):
+    return render(request, 'student/attendance.html',{'title':'Attendance'})
+
+def schedule(request):
+    return render(request, 'student/schedule.html',{'title':'Schedule'})
+
+
+
+
+
+
+def add_student(request):
+    if request.method == 'POST':
+        
+        Student(firstname=request.POST.get('fname'),
+                lastname=request.POST.get('lname'),
+                birthday=request.POST.get('birth'),
+                gender=request.POST.get('gender'),
+                address=request.POST.get('address'),
+                contactnumber=request.POST.get('contact'),
+                course_id= Course.objects.get(course=request.POST.get('course_id')),
+                ).save()
+        redirect('student/')
+    else:
+        return render(request,'student/student.html')
